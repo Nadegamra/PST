76d95b0a8d3f3783042c51b2a475f3689fc5d1ae
Karolis <karolis@localhost.localdomain>
2023-10-16 13:58:29 +0300

    5/7 tests complete

--- Backend.Test/AuthHandlerTests.cs
+++ Backend.Test/AuthHandlerTests.cs
@@ -56,40 +56,30 @@ namespace Backend.Test
 
         }
         [Fact]
-        public async void RegisterPhysicalLenderTest()
+        public async void RegisterTest()
         {
             // Arrange
             var dbMock = new DbContextMock();
             var handler = AuthHandlerMock.GetMock(dbMock);
             var mapper = AutomapperMock.GetMock();
             // Act
-
+            await handler.Register(new RegisterPhysical { FirstName = "Firstname", LastName = "Lastnameson", Email = "register_test@example.com", Username = "register_test@example.com", Password = "Password!!!" });
+            await handler.Register(new RegisterLegal { CompanyCode = "101010", CompanyName = "Test company", Email = "register_company@example.com", Username = "register_company@example.com", Password = "New!Password" });
             // Assert
-            Assert.Equal(1, 0);
-        }
-        [Fact]
-        public async void RegisterLegalLenderTest()
-        {
-            // Arrange
-            var dbMock = new DbContextMock();
-            var handler = AuthHandlerMock.GetMock(dbMock);
-            var mapper = AutomapperMock.GetMock();
-            // Act
+            var addedUsers = dbMock.Users.TakeLast(2).ToList();
+            Assert.Equal(6, addedUsers[0].Id);
+            Assert.Equal("Firstname", addedUsers[0].FirstName);
+            Assert.Equal("Lastnameson", addedUsers[0].LastName);
+            Assert.Equal("register_test@example.com", addedUsers[0].Email);
+            Assert.Equal("register_test@example.com", addedUsers[0].UserName);
+            Assert.Equal("Password!!!", addedUsers[0].PasswordHash);
 
-            // Assert
-            Assert.Equal(1, 0);
-        }
-        [Fact]
-        public async void RegisterAsBorrowerTest()
-        {
-            // Arrange
-            var dbMock = new DbContextMock();
-            var handler = AuthHandlerMock.GetMock(dbMock);
-            var mapper = AutomapperMock.GetMock();
-            // Act
-
-            // Assert
-            Assert.Equal(1, 0);
+            Assert.Equal(7, addedUsers[1].Id);
+            Assert.Equal("101010", addedUsers[1].CompanyCode);
+            Assert.Equal("Test company", addedUsers[1].CompanyName);
+            Assert.Equal("register_company@example.com", addedUsers[1].Email);
+            Assert.Equal("register_company@example.com", addedUsers[1].UserName);
+            Assert.Equal("New!Password", addedUsers[1].PasswordHash);
         }
         [Fact]
         public async void GetRegistrationRequestsTest()
